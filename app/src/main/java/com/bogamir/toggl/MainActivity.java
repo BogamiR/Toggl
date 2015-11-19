ccfef6d13e875e93ef40f6909dbbdcfb52f69ff2
BogamiR <shchavinskiyb@gmail.com>
2015-11-19 17:38:51 +0200

    .

--- app/src/main/java/com/bogamir/toggl/MainActivity.java
+++ app/src/main/java/com/bogamir/toggl/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -45,10 +47,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        /*//noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
@@ -57,10 +59,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.save:
-                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                Button btnNew = new Button(this);
-                btnNew.setText(editText.getText().toString());
-                Main.addView(btnNew, lParams);
+                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                CheckBox chb = new CheckBox(this);
+                TextView tv1 = new TextView(this);
+                TextView tv2 = new TextView(this);
+                Button btn1 = new Button(this);
+                Button btn2 = new Button(this);
+                Button btn3 = new Button(this);
+                TextView tv3 = new TextView(this);
+                TextView tv4 = new TextView(this);
+                tv1.setText(editText.getText().toString());
+
+                Main.addView(chb, lParams);
+                Main.addView(tv1, lParams);
+                Main.addView(tv2, lParams);
+                Main.addView(btn1, lParams);
+                Main.addView(btn2, lParams);
+                Main.addView(btn3, lParams);
+                Main.addView(tv3, lParams);
+                Main.addView(tv4, lParams);
                 break;
 
         }
