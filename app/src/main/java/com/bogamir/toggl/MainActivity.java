1af7ce18e3118eac627dd4e93e0963a1fefa0cbd
BogamiR <shchavinskiyb@gmail.com>
2015-11-19 16:30:01 +0200

    .

--- app/src/main/java/com/bogamir/toggl/MainActivity.java
+++ app/src/main/java/com/bogamir/toggl/MainActivity.java
@@ -1,15 +1,21 @@
 package com.bogamir.toggl;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout Main;
+    EditText editText;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Main = (LinearLayout) findViewById(R.id.Main);
+        editText = (EditText) findViewById(R.id.editText);
+        save = (Button) findViewById(R.id.save);
+
+        save.setOnClickListener(this);
     }
 
     @Override
@@ -39,5 +51,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.save:
+                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                Button btnNew = new Button(this);
+                btnNew.setText(editText.getText().toString());
+                Main.addView(btnNew, lParams);
+                break;
+
+        }
     }
 }
