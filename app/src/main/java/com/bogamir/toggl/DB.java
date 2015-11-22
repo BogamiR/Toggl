6e32a6d6777a96416077ebb81b2d9d5cc21341a7
BogamiR <shchavinskiyb@gmail.com>
2015-11-22 18:08:13 +0200

    .

---
+++ app/src/main/java/com/bogamir/toggl/DB.java
@@ -0,0 +1,103 @@
+package com.bogamir.toggl;
+
+        import android.content.ContentValues;
+        import android.content.Context;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.database.sqlite.SQLiteDatabase.CursorFactory;
+        import android.database.sqlite.SQLiteOpenHelper;
+
+public class DB {
+
+    private static final String DB_NAME = "mydb";
+    private static final int DB_VERSION = 1;
+    private static final String DB_TABLE = "mytab";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TV1= "tv1";
+    public static final String COLUMN_TV2= "tv2";
+    public static final String COLUMN_BTN1= "btn1";
+    public static final String COLUMN_TV3= "tv3";
+    public static final String COLUMN_TV4= "tv4";
+
+    private static final String DB_CREATE =
+            "create table " + DB_TABLE + "(" +
+                    COLUMN_ID + " integer primary key autoincrement, " +
+                    COLUMN_TV1 + " text, " +
+                    COLUMN_TV2 + " text, " +
+                    COLUMN_BTN1 + " integer, " +
+                    COLUMN_TV3 + " text, " +
+                    COLUMN_TV4 + " text" +
+                    ");";
+
+    private final Context mCtx;
+
+
+    private DBHelper mDBHelper;
+    private SQLiteDatabase mDB;
+
+    public DB(Context ctx) {
+        mCtx = ctx;
+    }
+
+    // открыть подключение
+    public void open() {
+        mDBHelper = new DBHelper(mCtx, DB_NAME, null, DB_VERSION);
+        mDB = mDBHelper.getWritableDatabase();
+    }
+
+    // закрыть подключение
+    public void close() {
+        if (mDBHelper!=null) mDBHelper.close();
+    }
+
+    // получить все данные из таблицы DB_TABLE
+    public Cursor getAllData() {
+        return mDB.query(DB_TABLE, null, null, null, null, null, null);
+    }
+
+    // добавить запись в DB_TABLE
+    public void addRec(String tv1, String tv2, int btn1, String tv3, String tv4) {
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_TV1, tv1);
+        cv.put(COLUMN_TV2, tv2);
+        cv.put(COLUMN_BTN1, btn1);
+        cv.put(COLUMN_TV3, tv3);
+        cv.put(COLUMN_TV4, tv4);
+        mDB.insert(DB_TABLE, null, cv);
+    }
+
+    // удалить запись из DB_TABLE
+    public void delRec(long id) {
+        mDB.delete(DB_TABLE, COLUMN_ID + " = " + id, null);
+    }
+
+    // класс по созданию и управлению БД
+    private class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context, String name, CursorFactory factory,
+                        int version) {
+            super(context, name, factory, version);
+        }
+
+        // создаем и заполняем БД
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DB_CREATE);
+
+            ContentValues cv = new ContentValues();
+            for (int i = 1; i < 5; i++) {
+                cv.put(COLUMN_TV1, "Name"+i);
+                cv.put(COLUMN_TV2, "Project"+i);
+                cv.put(COLUMN_BTN1, R.drawable.ic_play_arrow_black_18dp);
+                cv.put(COLUMN_TV3, i+"sec");
+                cv.put(COLUMN_TV4, "time");
+                mDB.insert(DB_TABLE, null, cv);
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+    }
+}
--- app/src/main/java/com/bogamir/toggl/MainActivity.java
+++ app/src/main/java/com/bogamir/toggl/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bogamir.toggl;
 
 import android.app.ActionBar;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -10,20 +14,20 @@ import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.database.Cursor;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.Timer;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, Runnable {
 
@@ -36,8 +40,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String timeStart;
     boolean flag = true;
     int time_calc = 0, j = 0;
-    List<View> allEds;
-    View view;
+    //List<View> allEds;
+    //View view;
+    DB db;
+    SimpleCursorAdapter scAdapter;
+    ListView lvData;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +54,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        linear = (LinearLayout) findViewById(R.id.linear);
+        db = new DB (this);
+        db.open();
+
+        cursor = db.getAllData();
+        startManagingCursor(cursor);
+
+        String[] from = new String[] { DB.COLUMN_TV1, DB.COLUMN_TV2, DB.COLUMN_BTN1, DB.COLUMN_TV3, DB.COLUMN_TV4 };
+        int[] to = new int[] { R.id.tv1, R.id.tv2, R.id.btn1, R.id.tv3, R.id.tv4 };
+        scAdapter = new SimpleCursorAdapter(this, R.layout.custom_layout, null, from, to, 0);
+
+        lvData = (ListView) findViewById(R.id.lvData);
+
+        //getSupportLoaderManager().initLoader(0, null, this);
+        //linear = (LinearLayout) findViewById(R.id.linear);
         editText = (EditText) findViewById(R.id.editText);
         selectProject = (EditText) findViewById(R.id.selectProject);
         timer = (TextView) findViewById(R.id.timer);
         go = (Button) findViewById(R.id.go);
-        allEds = new ArrayList<View>();
+        //allEds = new ArrayList<View>();
 
+        lvData.setAdapter(scAdapter);
+        registerForContextMenu(lvData);
         go.setOnClickListener(this);
-
     }
 
     @Override
@@ -87,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+
         switch (v.getId()) {
             case R.id.go:
                 if (flag) {
@@ -99,32 +122,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     timer.setText(R.string.timer);
                     go.setText(R.string.go);
 
-                    view = getLayoutInflater().inflate(R.layout.custom_layout, null);
+                    db.addRec(editText.getText().toString(), selectProject.getText().toString(), R.drawable.ic_play_arrow_black_18dp,
+                            time_calc + "sec", timeStart + " - " + time.format(new Date(System.currentTimeMillis())));
+                    cursor.requery();
+
+
+                    /*view = getLayoutInflater().inflate(R.layout.custom_layout, linear, false);
 
                     TextView tv1 = (TextView) view.findViewById(R.id.tv1);
                     TextView tv2 = (TextView) view.findViewById(R.id.tv2);
                     TextView tv3 = (TextView) view.findViewById(R.id.tv3);
                     TextView tv4 = (TextView) view.findViewById(R.id.tv4);
 
-
                     tv1.setText(editText.getText().toString());
                     tv2.setText(selectProject.getText().toString());
                     tv3.setText(time_calc + "sec");
-                    tv4.setText(timeStart + " - " + time.format(new Date(System.currentTimeMillis())));
+                    tv4.setText(timeStart + " - " + time.format(new Date(System.currentTimeMillis())));*/
 
-                    view.setId(j);
 
-                    allEds.add(view);
-                    registerForContextMenu(allEds.get(j));
+                    /*String[] from = new String[] {editText.getText().toString(), selectProject.getText().toString(), time_calc + "sec",
+                            timeStart + " - " + time.format(new Date(System.currentTimeMillis()))};*/
 
-                    linear.addView(view);
+
+                    //linear.addView(view);
+
+
+                    //view.setId(j);
+
+                    //allEds.add(view);
+                    //registerForContextMenu(allEds.get(j));
+
+
 
                     time_calc = 0;
                     j++;
                     flag = true;
                 }
                 break;
-            case R.id.btn2:
+            case R.id.btn1:
                 timeStart = time.format(new Date(System.currentTimeMillis()));
                 go.setText(R.string.stop);
                 flag = false;
@@ -135,22 +170,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
         menu.add(0, 0, 0, "Delete");
         menu.add(0, 1, 0, "Deleted all");
-        //super.onCreateContextMenu(menu, v, menuInfo);
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case 0:
-                for (Integer i = 0; i < allEds.size(); i++)
-                    if (view.getId() == i) {
-                        ((LinearLayout) allEds.get(i).getParent()).removeView(allEds.get(i));
-                        allEds.remove(i);
-                        Toast.makeText(MainActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
-                    }
-                break;
+                AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                db.delRec(acmi.id);
+                cursor.requery();
+                return true;
+                //break;
             case 1:
                 linear.removeAllViews();
                 Toast.makeText(MainActivity.this, "Deleted all", Toast.LENGTH_SHORT).show();
@@ -159,9 +192,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onContextItemSelected(item);
     }
 
+    protected void onDestroy(){
+        super.onDestroy();
+        db.close();
+    }
+
     @Override
     public void run() {
         timer.setText(time_calc++ + " sec");
         handler.postDelayed(this, 1000);
     }
+/*
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new MyCursorLoader(this, db);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        scAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    static class MyCursorLoader extends CursorLoader{
+        DB db;
+
+        public MyCursorLoader(Context context, DB db){
+            super(context);
+            this.db = db;
+        }
+
+        @Override
+        public Cursor loadInBackground(){
+            Cursor cursor = db.getAllData();
+            return cursor;
+        }
+    }*/
 }
